2d4688905a9083e65f110ed570f925e30d061a9f
franco-creator <francolosasso10@gmail.com>
2024-06-24 23:27:24 -0300

    configurada la pantalla en la agenda electronica

--- agenda_electronica/src/logica/agenda_electronica.java
+++ agenda_electronica/src/logica/agenda_electronica.java
@@ -2,19 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package agendaelectronica;
+package logica;
+
+import igu.Pantalla;
 
 /**
  *
  * @author franc
  */
-public class AgendaElectronica {
+public class agenda_electronica {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // lo primero que debo de hacer es cambiar la configuracion para que todo este conectado bien y demas
+        // TODO code application logic here
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
     }
     
 }
